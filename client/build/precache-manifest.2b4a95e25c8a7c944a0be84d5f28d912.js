bbcd8bc9785f9257d4969fbd7a57947aaac2066e
Minjeong <mindyeoi@khu.ac.kr>
2021-06-20 03:22:11 +0900

    plz

---
+++ client/build/precache-manifest.2b4a95e25c8a7c944a0be84d5f28d912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db74b67143ebd6d604290fa1181dd05a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "816cfabf4853ead8d600",
+    "url": "/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "f15c7b67fc4c8f1d307b",
+    "url": "/static/css/main.c9112ef1.chunk.css"
+  },
+  {
+    "revision": "816cfabf4853ead8d600",
+    "url": "/static/js/2.4d47edc8.chunk.js"
+  },
+  {
+    "revision": "f15c7b67fc4c8f1d307b",
+    "url": "/static/js/main.f04a225b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -33,21 +33,21 @@ app.use('/api/product', require('./routes/product'));
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  // index.html for all page routes
-  app.get("*", (req, res) => { 
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    res.send("hello world");
-  });
-}
-
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
 });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder
+  app.use(express.static(path.join("client/build")));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => { 
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
